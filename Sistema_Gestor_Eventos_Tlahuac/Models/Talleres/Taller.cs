e5beccd8e717e4ac67089442b508606df225520b
lilianagm7 <163457301+lilianagm7@users.noreply.github.com>
2026-03-16 10:13:30 -0600

    Programa ejecutando hasta lugares

--- Sistema_Gestor_Eventos_Tlahuac/Models/Talleres/Taller.cs
+++ Sistema_Gestor_Eventos_Tlahuac/Models/Talleres/Taller.cs
@@ -1,37 +1,29 @@
-﻿using Sistema_Gestor_Eventos_Tlahuac.Models.Catalogos;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Sistema_Gestor_Eventos_Tlahuac.Models
 {
-    public class Evento
+    public class Taller
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(150)]
         public string Nombre { get; set; }
-
         [StringLength(500)]
         public string? Descripcion { get; set; }
-
+        [Required]
+        public int CupoMaximo { get; set; }
         [Required]
         public DateTime FechaInicio { get; set; }
-
-        public DateTime? FechaFin { get; set; }
-
-
-        public int CapacidadMaxima { get; set; }
-
+        [Required]
+        public DateTime FechaFin { get; set; }
         public bool Activo { get; set; } = true;
 
 
         // Relaciones con las otras tablas
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
-
-        public int? TipoActividadId { get; set; }
-        public TipoActividad TipoActividad { get; set; }
-        public int LugarId { get; set; }
-        public Lugar Lugar { get; set; }
-        public ICollection<Inscripcion>? Inscripciones { get; set; }
+        public int TipoActividadId { get; set; }
+        public TipoActividad TipoActividades { get; set; }
     }
 }

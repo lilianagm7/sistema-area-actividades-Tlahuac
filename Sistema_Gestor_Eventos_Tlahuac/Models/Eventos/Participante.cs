3f294fd1231f105863589f0883149c9918861f3b
lilianagm7 <163457301+lilianagm7@users.noreply.github.com>
2026-03-15 19:48:12 -0600

    Creacion de controlador y vistas de Categorias

---
+++ Sistema_Gestor_Eventos_Tlahuac/Models/Eventos/Participante.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Gestor_Eventos_Tlahuac.Models
+{
+    public class Participante
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int EventoId { get; set; }
+
+        public Evento Evento { get; set; }
+
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
+        public bool Asistio { get; set; } = false;
+
+
+        //Relacion con la tabla usuarios
+        [Required]
+        public string UsuarioId { get; set; }
+        public ApplicationUser Usuario { get; set; }
+
+       
+    }
+}
